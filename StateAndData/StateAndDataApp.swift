bea52651051b6dcb61243ceb9cc06f0b86b550ae
tipachel88 <tipakoroche@gmail.com>
2022-02-02 02:14:13 +0300

    Initial Commit

---
+++ StateAndData/StateAndDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataApp.swift
+//  StateAndData
+//
+//  Created by Tipachel on 02.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
